d0a8a6b56c1e2d7035e5487b3dabee9711a9c530
Luca Mozzarelli <lucamozzarelli@gmail.com>
2017-01-28 16:27:15 +0100

    Initial Commit

---
+++ LMArticleViewController/LMArticleViewController.h
@@ -0,0 +1,19 @@
+//
+//  LMArticleViewController.h
+//  LMArticleViewController
+//
+//  Created by Luca Mozzarelli on 28/01/17.
+//  Copyright © 2017 Luca Mozzarelli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LMArticleViewController.
+FOUNDATION_EXPORT double LMArticleViewControllerVersionNumber;
+
+//! Project version string for LMArticleViewController.
+FOUNDATION_EXPORT const unsigned char LMArticleViewControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LMArticleViewController/PublicHeader.h>
+
+
